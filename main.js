1f41051b57b0db5012588f23209be204be93120d
Bruno Cozendey <brunocozendey@users.noreply.github.com>
2022-03-08 22:22:15 -0300

    First code commit

---
+++ main.js
@@ -0,0 +1,6 @@
+document.body.innerHTML = document.body.innerHTML.replace(/SALA-AVA-ILELgING474/g, 'Inglês');
+document.body.innerHTML = document.body.innerHTML.replace(/SALA13IMECOMPUT/g, 'SOII');
+document.body.innerHTML = document.body.innerHTML.replace(/SALA18IMECOMPUT/g, 'APS');
+document.body.innerHTML = document.body.innerHTML.replace(/SALA49IMECOMPUT/g, 'Aspectos Praticos CC');
+document.body.innerHTML = document.body.innerHTML.replace(/SALA51IMECOMPUT/g, 'Informatica na Educação');
+document.body.innerHTML = document.body.innerHTML.replace(/SALA61IMECOMPUT/g, 'SI');
